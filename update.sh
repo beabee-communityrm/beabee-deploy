cf723b9d9b9677f914a3bd03ce0862842a60734a
Will Franklin <will@willpf.co.uk>
2024-11-06 14:28:29 +0100

    Update update.sh script

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-docker-compose pull
-docker-compose run --rm --no-deps run npm run typeorm migration:run
-docker-compose up -d --remove-orphans
-docker-compose exec -T router nginx -s reload
-docker-compose exec -T app_router nginx -s reload
+docker compose pull
+docker compose run --rm --no-deps run npm run typeorm migration:run
+docker compose up -d --remove-orphans
+docker compose exec -T app_router nginx -s reload
